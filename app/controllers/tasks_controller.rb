b4a57a3481d99403d35646efd852c8b7c107dc48
Abhijith <ajk0x00@gmail.com>
2023-07-24 15:04:16 +0530

    Added API based architecture

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,8 +1,10 @@
 # frozen_string_literal: true
 
 class TasksController < ApplicationController
+  respond_to :html, :xml, :json
+
   def index
     @tasks = Task.all
-    render status :ok, json: { tasks: }
+    respond_with(@tasks)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,5 @@ Rails.application.routes.draw do
   resources :tasks, only: :index
 
   root "home#index"
-  get "/home/:id/:user" => "home#list"
   get "*path", to: "home#index", via: :all
 end
