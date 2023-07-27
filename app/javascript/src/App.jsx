7473440f65a6de7dd5108243389cbe2d57defe5d
Abhijith <ajk0x00@gmail.com>
2023-07-27 20:50:30 +0530

    commit

--- app/javascript/src/App.jsx
+++ app/javascript/src/App.jsx
@@ -1,26 +1,34 @@
 import React, { useEffect, useState } from "react";
 
 import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
 
-import { setAuthHeaders } from "apis/axios";
+import { registerIntercepts, setAuthHeaders } from "apis/axios";
 import { initializeLogger } from "common/logger";
-
-import Dashboard from "./components/Dashboard";
-import CreateTask from "./components/Tasks/Create";
+import Dashboard from "components/Dashboard";
+import PageLoader from "components/PageLoader";
+import CreateTask from "components/Tasks/Create";
 
 const App = () => {
   const [loading, setLoading] = useState(true);
+
   useEffect(() => {
     initializeLogger();
+    registerIntercepts();
     setAuthHeaders(setLoading);
   }, []);
 
   if (loading) {
-    return <h1>Loading...</h1>;
+    return (
+      <div className="h-screen">
+        <PageLoader />
+      </div>
+    );
   }
 
   return (
     <Router>
+      <ToastContainer />
       <Switch>
         <Route exact component={CreateTask} path="/tasks/create" />
         <Route exact component={Dashboard} path="/dashboard" />
